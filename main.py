9114e4463c4a69d3f3716c5c03cd847fff8f18e1
Takashi Ando <dodo5522@gmail.com>
2022-07-24 16:49:22 +0900

    Python3対応しつつ、コマンドライン実行可能に

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from __future__ import unicode_literals
-
 from collections import defaultdict
-import ConfigParser
 from datetime import datetime
+from subprocess import run, PIPE
 import glob
 import json
 import re
@@ -10,14 +8,16 @@ import shutil
 import string
 import sys
 import os
+from configparser import ConfigParser
 
 
+PATH_TO_EXIF_TOOL = '/usr/local/bin/exiftool'
 DEFAULT_OUTPUT_BASE_PATH = os.path.join(os.environ.get('HOME'), 'OutBox')
 DEFAULT_FILENAME_FORMAT = (
     '{y}{m}{d}/{Model}/{y}{m}{d}{H}{M}{S}-{bn}.{FileTypeExtension}')
 
 
-def parse_datetime(createdate):
+def parse_datetime(createdate: str) -> dict:
     """CreatedDateを扱いやすい形に分解する."""
     try:
         cdt = datetime.strptime(createdate[:19], '%Y:%m:%d %H:%M:%S')
@@ -41,7 +41,7 @@ def parse_datetime(createdate):
         )
 
 
-def branch_no(output_base_path, filename_format, photo_info):
+def branch_no(output_base_path: str, filename_format: str, photo_info) -> int:
     """枝番を求める.
 
     同一ファイル名のファイルがある場合枝番をカウントアップする.
@@ -64,10 +64,10 @@ def branch_no(output_base_path, filename_format, photo_info):
     return bn + 1
 
 
-def load_configure(source_dir):
+def load_configure(source_dir: str) -> tuple:
     """入力フォルダにあるsetting.iniファイルより出力設定を読み込む"""
     config_file = os.path.join(source_dir, 'setting.ini')
-    config = ConfigParser.SafeConfigParser({
+    config = ConfigParser({
         'output_photo_base': DEFAULT_OUTPUT_BASE_PATH,
         'output_video_base': DEFAULT_OUTPUT_BASE_PATH,
         'filename_format': DEFAULT_FILENAME_FORMAT,
@@ -79,11 +79,11 @@ def load_configure(source_dir):
     return filename_format, output_photo_base, output_video_base
 
 
-def copy_outbox(exif, output_base_path, filename_format):
+def copy_outbox(exif: dict, output_base_path: str, filename_format: str):
     """写真のexif属性により適切なフォルダへファイルをコピーする"""
     photo_info = defaultdict(lambda: 'Unknown')
     photo_info.update({
-        k: v.replace(' ', '_') if isinstance(v, basestring) else v
+        k: v.replace(' ', '_') if isinstance(v, str) else v
         for k, v in exif.items()
     })
     photo_info.update(parse_datetime(
@@ -107,14 +107,34 @@ def copy_outbox(exif, output_base_path, filename_format):
     shutil.copyfile(exif.get('SourceFile'), new_path)
 
 
-def is_movie(exif):
+def is_movie(exif: dict) -> bool:
     n = len(list(filter(lambda k: 'FrameRate' in k, exif.keys())))
-    return n &gt; 0
+    return n > 0
 
 
-def main(source_dir, input_json):
+def make_exif_json(source_dir: str, path_to_exif_tool: str) -> str:
+    """exiftoolを実行し、出力されたjsonファイルへのパスを返す"""
+    log_root_dir = '/tmp/photo_organizer'
+    log_file_base = f'{log_root_dir}/{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+    json_out = f'{log_file_base}.json'
+
+    os.makedirs(log_root_dir, exist_ok=True)
+
+    with open(f'{log_file_base}_cmd.log', mode='w') as f:
+        f.write(f'/usr/local/bin/exiftool -r -j {source_dir} > {json_out}')
+
+    res = run(f'{path_to_exif_tool} -r -j {source_dir} > {json_out} 2> {log_file_base}_err.log',
+        shell=True,
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+
+    return json_out if res.returncode == 0 else ''
+
+
+def main(source_dir: str, input_json: str) -> None:
     """exiftoolが出力するJSONファイルの属性から写真を整理する"""
-    exifs = json.loads(input_json)
+    exifs: list = json.loads(input_json)
     configurations = load_configure(source_dir)
     filename_format, output_photo_base, output_video_base = configurations
     for exif in exifs:
@@ -128,6 +148,25 @@ def main(source_dir, input_json):
 
 
 if __name__ == '__main__':
-    source_dir, input_json = sys.stdin.read().strip().split(',')
-    main(source_dir, open(input_json, 'r').read())
+    from argparse import ArgumentParser, Namespace
 
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-s', '--source-dir',
+        type=str,
+    )
+    parser.add_argument(
+        '-e', '--path-to-exif-tool',
+        type=str,
+        default=PATH_TO_EXIF_TOOL,
+    )
+
+    args: Namespace = parser.parse_args()
+
+    if args.source_dir:
+        source_dir = args.source_dir
+        input_json = make_exif_json(source_dir, args.path_to_exif_tool)
+    else:
+        source_dir, input_json = sys.stdin.read().strip().split(',')
+
+    main(source_dir, open(input_json, 'r').read())
